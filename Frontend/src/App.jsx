fb8f7632f1dbea61746c90fe4a35b6a95a72858a
Parvesh Kumar Sharma <parveshsharma8910@gmail.com>
2026-06-05 12:41:28 +0530

    home-basic

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar.jsx";
 import Home from "../pages/Home.jsx";
+import "./App.css";
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <div style={{ marginLeft: "76px", padding: "24px" }}>
+      <div style={{ marginLeft: "76px" }}>
         <Home />
       </div>
     </>
