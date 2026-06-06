5db759a2017dc6e2d657263c9e266c10dc9277db
Parvesh Kumar Sharma <parveshsharma8910@gmail.com>
2026-06-06 14:21:50 +0530

    rebuild

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar.jsx";
 import Home from "../pages/Home.jsx";
 import "./App.css";
-
+import { ReactLenis } from 'lenis/react'
 function App() {
 
   return (
     <>
+      <ReactLenis root />
       <Navbar />
       <div style={{ marginLeft: "76px" }}>
         <Home />
